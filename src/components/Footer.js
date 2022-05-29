ccd78467e657ac636973d38acd7dbbbe3d5c0fed
Jennifer Nguyen <newyenjay@gmail.com>
2022-05-29 00:01:35 -0600

    Removing unused imports

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,8 @@
 import React, { Component } from "react";
 import ContactForm from "./ContactForm";
-import FunFact from "./FunFact";
 
 class Footer extends Component {
   render() {
-    if (this.props.sharedBasicInfo) {
-      var networks = this.props.sharedBasicInfo.social.map(function(network) {
-        return (
-          <span key={network.name} className="m-4">
-            <a href={network.url} target="_blank" rel="noopener noreferrer">
-              <i className={network.class}></i>
-            </a>
-          </span>
-        );
-      });
-    }
-
     return (
       <footer>
         <div className="row">
